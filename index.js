e3569fc7b9b28cae84ad5ffcb1749e81ae926719
Will <wfoster12@gmail.com>
2020-09-09 21:25:33 +0100

    mongoose and DB connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
