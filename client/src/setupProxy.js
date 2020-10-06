c93a87291f02ed6afd699fbf8c4b55c800867984
Will <wfoster12@gmail.com>
2020-10-06 20:37:36 +0100

    Proxy update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,3 +1,5 @@
+//creates proxy to route links to correct domain
+//TODO: https://powerful-brushlands-87033.herokuapp.com/
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
